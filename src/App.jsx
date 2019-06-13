185f3f6a4eecbd96b076bc08ee1c63f180e1eaab
Ted Weiss <theodore_weiss@outlook.com>
2019-06-12 22:02:34 -0400

    make App a function not a class

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        To get started, edit <code>src/App.jsx</code> and save to reload.
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className='App'>
+      To get started, edit <code>src/App.jsx</code> and save to reload.
+    </div>
+  )
 }
 
 export default App
